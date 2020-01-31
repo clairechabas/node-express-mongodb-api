72e5f8b73321c0e38ac17fb76aae28ee88a53c63
Claire Chabas <claire.chabas@gmail.com>
2020-01-31 19:55:17 +0100

    Adding endpoint and use for socks routes

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Adding middlewares
 app.use(express.json());
 
+// Route to server root
 app.get('/', (req, res) => {
   res.send('Welcome to a happy socks world !');
 });
+
+// Socks routes entry
+const socksRoutes = require('./routes/socks');
+app.use('/api/socks', socksRoutes);
 
 app.listen(port, console.log(`Listening on port ${port}`));
