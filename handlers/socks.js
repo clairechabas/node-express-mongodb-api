92f0af815e9144bd317fbc5ef80ee6c62d308c63
Claire Chabas <claire.chabas@gmail.com>
2020-01-31 19:59:05 +0100

    Initiating CRUD handlers for socks

---
+++ handlers/socks.js
@@ -0,0 +1,16 @@
+const db = require('./models/socks');
+
+// Create a pair of socks
+exports.addSocks = (req, res) => {};
+
+// Get all socks
+exports.getAllSocks = (req, res) => {};
+
+// Get one pair of socks
+exports.getAPairOfSocks = (req, res) => {};
+
+// Update a pair of socks
+exports.updateAPairOfSocks = (req, res) => {};
+
+// Delete a pair of socks
+exports.deleteAPairOfSocks = (req, res) => {};
